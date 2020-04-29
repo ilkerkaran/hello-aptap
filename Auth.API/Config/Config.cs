a0d922dc54f95368e637b6ca9be638444f110f0b
ilkerkaran <ilkerkaran@gmail.com>
2020-04-30 00:04:21 +0100

    Auth API added

---
+++ Auth.API/Config/Config.cs
@@ -0,0 +1,59 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Auth.API.Conf
+{
+    public class Config
+    {
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("Business.API", "Business API")     ,
+                new ApiResource("Gateway.API", "Gateway API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "react-client",
+                    ClientName = "SPA Client",
+                    ClientUri = "https://localhost:3000",
+                    RequireConsent = false,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =
+                    {
+                        "https://localhost:3000/",
+                        "https://localhost:3000/"
+                    },
+
+                    PostLogoutRedirectUris = { "https://localhost:3000/" },
+                    AllowedCorsOrigins = { "https://localhost:3000" },
+
+                    AllowedScopes = {"gateway.api"}
+                }
+            };
+        }
+
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+    }
+
+
+
+
+
+}
---
+++ Auth.API/Startup.cs
@@ -0,0 +1,63 @@
+using Auth.API.Conf;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Auth.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+
+
+
+            builder.AddDeveloperSigningCredential();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            // Adds IdentityServer
+            app.UseIdentityServer();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
